b4d3066153b3b78e5337e2af3e26eb7103461bc8
gh0st <maratka2202@gmail.com>
2025-02-05 10:13:54 +0300

    Vue project init

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <div class="container">
+
+        </div>
+    </header>
+
+    <main class="main">
+        <div class="container">
+
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="container"></div>
+    </footer>
+</template> 
